98edc11e6567abbfb3d314d3b020cc4c9052ffe7
Danieldiaz Smakov <d.smakov@astanait.edu.kz>
2021-06-15 17:05:20 +0600

    Add business logic validation mechanisms

---
+++ Reestr.BLL/Validation/ModelStateWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Reestr.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.ModelBinding;
+
+namespace Reestr.BLL.Validation
+{
+    public class ModelStateWrapper : IValidationDictionary
+    {
+        private ModelStateDictionary _modelState;
+        public ModelStateWrapper(ModelStateDictionary modelState)
+        {
+            _modelState = modelState;
+        }
+        public void AddError(string key, string errorMessage)
+        {
+            _modelState.AddModelError(key, errorMessage);
+        }
+        public bool IsValid
+        {
+            get => _modelState.IsValid;
+        }
+    }
+}
